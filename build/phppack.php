d0d9003a235142de397f7d2241fdff0ed1614be0
qinuoyun <qinuoyun@qq.com>
2018-11-14 23:57:17 +0800

    初始化构架打包框架

---
+++ build/phppack.php
@@ -0,0 +1,15 @@
+<?php
+namespace phppacks\phppack;
+/**
+ * This7 Frame
+ * @Author: else
+ * @Date:   2018-01-11 14:04:08
+ * @Last Modified by:   else
+ * @Last Modified time: 2018-01-12 02:23:05
+ */
+
+class phppack {
+    public function demo($value = '') {
+        echo "phppack";
+    }
+}
